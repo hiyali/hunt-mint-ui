43c3a8bc3eaecf39f148800381e360c397a121a3
Salam Hiyali <hiyali920@gmail.com>
2017-03-06 17:36:27 +0800

    toast changed!!!

--- packages/toast/src/toast.vue
+++ packages/toast/src/toast.vue
@@ -1,8 +1,12 @@
 <template>
   <transition name="mint-toast-pop">
-    <div class="mint-toast" v-show="visible" :class="customClass" :style="{ 'padding': iconClass === '' ? '10px' : '20px' }">
-      <i class="mint-toast-icon" :class="iconClass" v-if="iconClass !== ''"></i>
-      <span class="mint-toast-text" :style="{ 'padding-top': iconClass === '' ? '0' : '10px' }">{{ message }}</span>
+    <div class="mint-toast" v-show="visible">
+      <div class="mint-toast-wrapper" :class="customClass" :style="{ 'padding': iconClass === '' ? '10px' : '20px' }">
+        <i class="mint-toast-icon" :class="iconClass" v-if="iconClass !== ''"></i>
+        <span class="mint-toast-text" :style="{ 'padding-top': iconClass === '' ? '0' : '10px' }">{{ message }}</span>
+      </div>
+
+      <div class="mint-toast-mask" @touchmove.stop.prevent @click="ownClose"></div>
     </div>
   </transition>
 </template>
@@ -10,48 +14,61 @@
 <style>
   @component-namespace mint {
     @component toast {
-      position: fixed;
-      max-width: 80%;
-      border-radius: 5px;
-      background: rgba(0, 0, 0, 0.7);
-      color: #fff;
-      box-sizing: border-box;
-      text-align: center;
-      z-index: 1000;
+      position: relative;
+      z-index: 200;
       transition: opacity .3s linear;
-  
+
+      @descendent wrapper {
+        position: fixed 50% * * 50%;
+        transform: translate(-50%, -50%);
+
+        max-width: 80%;
+        border-radius: 5px;
+        background: rgba(0, 0, 0, 0.7);
+        color: #fff;
+        box-sizing: border-box;
+        text-align: center;
+      }
+
       @descendent icon {
         display: block;
         text-align: center;
         font-size: 56px;
       }
-      
+
       @descendent text {
         font-size: 14px;
         display: block;
         text-align: center;
       }
-      
+
       @when placetop {
         top: 50px;
         left: 50%;
         transform: translate(-50%, 0);
       }
-      
+
       @when placemiddle {
         left: 50%;
         top: 50%;
         transform: translate(-50%, -50%);
       }
-      
+
       @when placebottom {
         bottom: 50px;
         left: 50%;
         transform: translate(-50%, 0);
       }
-      
+
       @descendent pop-enter, pop-leave-active {
         opacity: 0;
+      }
+
+      @descendent mask {
+        position: fixed 0 * * 0;
+        size: 100%;
+        opacity: 0;
+        background: transparent;
       }
     }
   }
@@ -72,6 +89,12 @@
       iconClass: {
         type: String,
         default: ''
+      }
+    },
+
+    methods: {
+      ownClose: function() {
+        this.close();
       }
     },
 
